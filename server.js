82e8ee89007911fbc1cb08c23b40542bb6a4a83d
Robert <basehintz@gmail.com>
2020-11-03 11:43:55 -0700

    stopping point from last night, working on log in but unable to post account

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const methodOverride = require("method-override");
 const mongoose = require("mongoose");
 const app = express();
 const db = mongoose.connection;
+require("dotenv").config();
+
 //___________________
 //Port
 //___________________
@@ -31,6 +33,13 @@ db.on("disconnected", () => console.log("mongo disconnected"));
 // open the connection to mongo
 db.on("open", () => {});
 
+//set up to have routes in controllers/weightData
+const weightDataController = require("./controllers/weightData.js");
+app.use("/index", weightDataController);
+
+// set up to have routes in controller/users
+const userController = require("./controllers/users.js");
+app.use("/users", userController);
 //========================================\\
 //               Middleware               \\
 //========================================\\
@@ -44,9 +53,6 @@ app.use(express.json()); // returns middleware that only parses JSON - may or ma
 
 //use method override
 app.use(methodOverride("_method")); // allow POST, PUT and DELETE from a form
-
-const weightDataController = require("./controllers/weightData.js");
-app.use("/index", weightDataController);
 
 //========================================\\
 //               listener                 \\
